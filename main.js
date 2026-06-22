5ba8729d0dea1c79e43451f72c843fd943342483
denissjurjaanss-lgtm <denissjurjaanss@gmail.com>
2026-06-23 01:05:06 +0300

    Init: Electron main process

---
+++ main.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    icon: path.join(__dirname, 'assets/icon.png')
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startUrl);
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// IPC komunikācija
+ipcMain.handle('save-user-data', async (event, userData) => {
+  // Saglabāt lietotāja datus
+  return { success: true };
+});
+
+ipcMain.handle('get-user-data', async (event) => {
+  // Iegūt lietotāja datus
+  return {};
+});
